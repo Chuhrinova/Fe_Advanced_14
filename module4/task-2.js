35cbf0c8fb7b2332461f7317309dc7f6600e82b6
Natasha <litvinenko2262@gmail.com>
2019-08-15 12:17:11 +0300

    Homework_4module_4

---
+++ module4/task-2.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const inventory = {
+  items: ["Монорельса", "Фильтр"],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  }
+};
+inventory.add("Аптечка");
+console.log(inventory.items);
+
+inventory.remove("Фильтр");
+console.log(inventory.items);
+
+const invokeInventoryOperation = function(itemName, inventoryAction) {
+  for (let i = 0; i < inventory.length; i++) {
+    inventoryAction(itemName[i]);
+  }
+
+  console.log(`Invoking ${inventoryAction.name} operation on ${itemName}`);
+};
+
+invokeInventoryOperation("Аптечка", inventory.add);
+
+invokeInventoryOperation("Фильтр", inventory.remove);
