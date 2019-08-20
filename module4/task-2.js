d79eccfb97ca6b47d05e99b03bf42a370d7312d9
Natasha <litvinenko2262@gmail.com>
2019-08-20 11:32:07 +0300

    Homework_4_added_changes

--- module4/task-2.js
+++ module4/task-2.js
@@ -9,20 +9,16 @@ const inventory = {
     this.items = this.items.filter(item => item !== itemName);
   }
 };
-inventory.add("Аптечка");
-console.log(inventory.items);
-
-inventory.remove("Фильтр");
-console.log(inventory.items);
 
 const invokeInventoryOperation = function(itemName, inventoryAction) {
-  for (let i = 0; i < inventory.length; i++) {
-    inventoryAction(itemName[i]);
-  }
-
-  console.log(`Invoking ${inventoryAction.name} operation on ${itemName}`);
+  console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+  inventoryAction.call(inventory, itemName);
 };
 
 invokeInventoryOperation("Аптечка", inventory.add);
 
+console.log(inventory.items);
+
 invokeInventoryOperation("Фильтр", inventory.remove);
+
+console.log(inventory.items);
